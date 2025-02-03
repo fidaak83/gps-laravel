4b125edf306a8f2916c380dfa29cc5d3d26f3194
fidaak83 <bdcrak-fidaak1983@yahoo.com>
2025-02-03 17:24:24 +0300

    Updates

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Codec8Controller;
+use App\Livewire\Counter;
+use App\Livewire\Home;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+ 
+Route::get('/counter', Counter::class);
+Route::get('/home', Home::class);
 
 
 Route::post('/parse', [Codec8Controller::class, 'parse']);
